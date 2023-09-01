05ea402b62d182d3d998b42fc8f074363134a7ee
Pavanelli99 <pavanelli1999@gmail.com>
2023-08-31 20:29:39 -0400

    Aula_31_08_fim

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,87 +2,92 @@
 /* eslint-disable no-useless-constructor */
 //import logo from "./logo.svg";
 import "./styles.css";
-import { Component } from "react";
 import { loadPosts } from "../../utils/load-posts";
 import { PostCard } from "../PostCard";
 import { Button } from "../Button";
+import { useCallback, useEffect, useState } from "react";
 
-class Home extends Component {
-
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postPerPage: 4,
-  };
-
-  componentDidMount() {
-    this.loadPosts();
-
-  }
-
-  loadPosts = async () => {
-    const { page, postPerPage } = this.state;
-    const photosAndPosts = await loadPosts()
-
-    this.setState({
-       posts: photosAndPosts.slice(page, page + postPerPage),
-       allPosts: photosAndPosts
-      
-      });
-  };
-
-
-  loadMorePosts = async () => {
-    const {page, postsPerPage, allPosts, post} = this.state
-
-    const nextPage = page + postsPerPage
-
-    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage)
-
-    this.setState({posts: [...post, ...nextPosts], page: nextPage}); //... significa destructor
-     
-
-  };
-
-  handleSearch = (e)=> { // Pesquisar
-    const {value} = e.target;
-    this.setState({serachvalue: value});
-    console.log(value);
-
-  };
-
-  render() {
-    const { posts , serachvalue } = this.state;
-
-    const filteredPosts = !!serachvalue //operador de circuito
+export const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postPerPage] = useState(5);
+  const [searchValue, setsearchValue] = useState("");
+  //------------------------------------------------------------------------------------------------
+  const filteredPosts = !!searchValue
     ? posts.filter((post) => {
-       return post.title.toLowerCase().includes(post.title.toLowerCase())
-    })
-    :posts;
+        return post.title.toLowerCase().includes(searchValue.toLowerCase());
+      })
+    : posts;
 
-    return (
-      <section className="container"> 
-        <input
+  // componentDidMount() {
+  //   this.loadPosts();
+
+  // }
+  //------------------------------------------------------------------------------------------------
+  const handleLoadPosts = useCallback(async (page, postPerPage) => { 
+    // const { page, postPerPage } = this.state;
+    const photosAndPosts = await loadPosts();
+
+    setPosts(photosAndPosts.slice(page, postPerPage));
+    setAllPosts(photosAndPosts);
+  }, []);
+  //------------------------------------------------------------------------------------------------
+  const loadMorePosts = () => {
+    // const {page, postsPerPage, allPosts, post} = this.state;
+
+    const nextPage = page + postPerPage;
+
+    const nextPosts = allPosts.slice(nextPage, nextPage + postPerPage);
+
+    setPosts([...posts, ...nextPosts]);
+    setPage(nextPage);
+
+    // this.setState({posts: [...post, ...nextPosts], page: nextPage}); //... significa destructor
+  };
+  //-----------------------------------------------------------------------------------------------
+  const handleSearch = (e) => {
+    // Pesquisar
+    const { value } = e.target;
+    setsearchValue(value);
+    // this.setState({serachvalue: value});
+    // console.log(value);
+  };
+  //------------------------------------------------------------------------------------------------
+  useEffect(() => {
+    handleLoadPosts(0, postPerPage); // FUNÇÃO
+  }, [handleLoadPosts, postPerPage]); // DEPENDENCIA
+  //------------------------------------------------------------------------------------------------
+  return (
+    <section className="container">
+      <input
         type="text"
-        name ="txtSearch"
-        id = "txtSearch"
+        name="txtSearch"
+        id="txtSearch"
         placeholder="Search..."
-        onChange={this.handleSearch}
-        value={serachvalue}
-        />
+        onChange={handleSearch}
+        value={searchValue}
+      />
 
-
-        <div className="posts">
+      <div className="posts">
         {filteredPosts.map((post) => (
-          <PostCard Key = {post.id} post = {post}/>
-
+          <PostCard Key={post.id} post={post} />
         ))}
-        </div>
-        <Button text = "Load more posts"  action ={this.loadMorePosts}/>
-      </section>
-    );
-  }
-}
+      </div>
+      <Button text="Load more posts" action={loadMorePosts} />
+    </section>
+  );
+
+  // render() {
+  //   const { posts , serachvalue } = this.state;
+
+  //   const filteredPosts = !!serachvalue //operador de circuito
+  //   ? posts.filter((post) => {
+  //      return post.title.toLowerCase().includes(post.title.toLowerCase())
+  //   })
+  //   :posts;
+
+  // }
+};
 
 export default Home;
